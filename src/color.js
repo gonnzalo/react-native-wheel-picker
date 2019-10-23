ebcf3d8bcb1be5e5b67711d72b26c0ebb89b59c5
Gonzalo <arecogonzalo@gmail.com>
2019-10-23 12:55:11 +0800

    change directories

---
+++ src/color.js
@@ -0,0 +1,10 @@
+export default {
+  background: '#f3f3f5',
+  navegation: '#666',
+  dark: '#58585a',
+  darkMedium: '#9f9fa1',
+  darkLigth: '#d6d6d8',
+  primary: '#13a89e',
+  white: '#fff',
+  secundaryDark: '#555',
+};
